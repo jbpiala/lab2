a265f584f2b47ddea7db25d8b099c17031627ed2
John Emmanuel Piala <jbpiala@student.apc.edu.ph>
2024-01-13 16:12:49 +0800

    added Operators JavaScript

--- week5/script.js
+++ week5/script.js
@@ -4,3 +4,11 @@ document.getElementById("demo").innerHTML = d.toDateString();
 let text1 = "Hello, ";
 text1 += "My Name is";
 document.getElementById("intro").innerHTML = text1;
+
+
+let text =
+
+`My hobbies include sketching, reading manhuas/manhwas/mangas, photography, playing sports, and gaming.
+I have a strong interests in arts, particularly photography, sketching, and animation, as well as martial arts.`;
+
+document.getElementById("itext").innerHTML = text;
