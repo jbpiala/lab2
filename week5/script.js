6b8da035a56cd176a1a2a7763e78e491794847f0
John Emmanuel Piala <jbpiala@student.apc.edu.ph>
2024-01-13 20:40:46 +0800

    added Lesson 2: Variables

--- week5/script.js
+++ week5/script.js
@@ -1,3 +1,4 @@
+
 const d = new Date();
 document.getElementById("demo").innerHTML = d.toDateString();
 
@@ -5,10 +6,13 @@ let text1 = "Hello, ";
 text1 += "My Name is";
 document.getElementById("intro").innerHTML = text1;
 
-function bg(){
-document.querySelector('body').style.background = "url('bg.jpg') center center / cover"
+let Name = "John Emmanuel Piala";
+document.getElementById("demo1").innerHTML = Name;
+
+function bg() {
+    document.querySelector('body').style.background = "url('bg.jpg') center center / cover";
 }
 
-function bg2(){
-document.querySelector('body').style.background = "url('bg2.jpg') center center / cover"
+function bg2() {
+    document.querySelector('body').style.background = "url('bg2.jpg') center center / cover";
 }
