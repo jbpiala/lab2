f2cb36fa0532a18e17bd79363b119f2f7393d7ee
John Emmanuel Piala <jbpiala@student.apc.edu.ph>
2024-01-13 15:45:19 +0800

    added String JavaScript

--- week5/script.js
+++ week5/script.js
@@ -1,2 +1,6 @@
 const d = new Date();
 document.getElementById("demo").innerHTML = d.toDateString();
+
+let text1 = "Hello, ";
+text1 += "My Name is";
+document.getElementById("intro").innerHTML = text1;
